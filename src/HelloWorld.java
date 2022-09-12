814910805ccaa3c350d4004a4e8061ce2d585e81
Selami <drgungordu@gmail.com>
2022-09-12 16:13:00 -0400

    Hello 2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
+// Doing some work here
 
+
+        System.out.println("Hello");
+        System.out.println("This is a trailer for git and github");
     }
 }
